bb33427df40abf6cfcd98899d05fb3d78b3b4fc9
Tung Hoang <tung@ras-aries.weworkers.io>
2019-10-14 17:47:44 -0700

    updated notebooks

--- run_docker.sh
+++ run_docker.sh
@@ -5,5 +5,5 @@ docker run -it --rm --runtime=nvidia \
     -v /home/tung/playground:/home/tung/playground \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -p 9094:6014 -p 5010:5010 \
-    -e NVIDIA_VISIBLE_DEVICES=7 -e CUDA_DEVICE_ORDER=PCI_BUS_ID \
+    -e NVIDIA_VISIBLE_DEVICES=6 -e CUDA_DEVICE_ORDER=PCI_BUS_ID \
     delf/nvcr.io/nvidia/tensorflow:19.09-py3 
